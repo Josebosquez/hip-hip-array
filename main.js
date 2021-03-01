febb434a592a7b5866f6f223e607f20536a9682b
Jose Bosquez <jose.bosquez@codeimmersives.com>
2021-03-01 16:21:07 -0700

    1 & 2

--- main.js
+++ main.js
@@ -18,11 +18,14 @@ const students = [
   'Patrick',
   'Steve',
 ];
-
+for (const item of students){
+  console.log(item);
+}
 
 // Challenge 2
 // Loop through the following array BACKWARDS, console.logging out each value. 64 should be printed first and 100 last.
 const grades = [100, 80, 110, 75, 83, 64];
+  console.log('value:', grades.reverse())
 
 
 // Challenge 3
